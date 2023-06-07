ffbe805c67219d95940690b7a2af11541b446fa8
hesdin <hesdin01@gmail.com>
2023-06-07 11:31:06 +0800

    t_uutap to integer

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,17 +6,19 @@ use Illuminate\Http\Request;
 use DB;
 use App\Helpers\Response;
 use Validator;
+
 class DataController extends Controller
 {
-    public function getDaftar(){
+    public function getDaftar()
+    {
         try {
-        $params = request('npwpd');
-        $query = DB::table('t_daftar');
+            $params = request('npwpd');
+            $query = DB::table('t_daftar');
 
-        if ($params) {
-            $query->where('npwpd', $params);
-        }
-        $data = $query->get();
+            if ($params) {
+                $query->where('npwpd', $params);
+            }
+            $data = $query->get();
 
             return Response::success($data, 'Daftar pajak has been get successfull');
         } catch (\Throwable $th) {
@@ -24,8 +26,9 @@ class DataController extends Controller
         }
     }
 
-    public function storeDaftar(Request $request){
-         $validator = Validator::make($request->all(), [
+    public function storeDaftar(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'npwpd' => 'required|unique:t_daftar',
             'nama_bu' => 'required',
             'alamat_bu' => 'required',
@@ -33,15 +36,14 @@ class DataController extends Controller
             'alamat_wp' => 'required',
             'no_telp' => 'required|numeric'
         ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
         }
 
         try {
-           
-          $data = DB::table('t_daftar')->insert([
-                 'npwpd' => $request->npwpd,
+
+            $data = DB::table('t_daftar')->insert([
+                'npwpd' => $request->npwpd,
                 'nama_bu' => $request->nama_bu,
                 'alamat_bu' => $request->alamat_bu,
                 'nama_wp' => $request->nama_wp,
@@ -49,13 +51,14 @@ class DataController extends Controller
                 'no_telp' => $request->no_telp
             ]);
 
-           return Response::success($data, 'Category has been create successfull');
+            return Response::success($data, 'Category has been create successfull');
         } catch (QueryException $e) {
             return Response::error(null, 'Category failed create');
         }
     }
 
-    public function getUuData(){
+    public function getUuData()
+    {
         try {
             $data = DB::table('t_uudata')->get();
             return Response::success($data, 'Daftar pajak has been get successfull');
@@ -64,7 +67,22 @@ class DataController extends Controller
         }
     }
 
-    public function get_uutap(){
+    public function get_uutap()
+    {
+        // try {
+        //     $params = request('npwpd');
+        //     $query = DB::table('t_uutap');
+
+        //     if ($params) {
+        //         $query->where('npwpd_gab', $params);
+        //     }
+
+        //     $data = $query->get();
+        //     return Response::success($data, 'Daftar pajak has been get successfull');
+        // } catch (\Throwable $th) {
+        //     return Response::error(null, 'UUtap failed get');
+        // }
+
         try {
             $params = request('npwpd');
             $query = DB::table('t_uutap');
@@ -72,11 +90,19 @@ class DataController extends Controller
             if ($params) {
                 $query->where('npwpd_gab', $params);
             }
-            
+
             $data = $query->get();
-            return Response::success($data, 'Daftar pajak has been get successfull');
+
+            // Mengubah tipe data field jumlah_gab dan total_gab menjadi integer
+            $data->transform(function ($item) {
+                $item->jumlah_gab = (int) $item->jumlah_gab;
+                $item->total_gab = (int) $item->total_gab;
+                return $item;
+            });
+
+            return Response::success($data, 'Daftar pajak has been get successfully');
         } catch (\Throwable $th) {
-            return Response::error(null, 'UUtap failed get');
+            return Response::error(null, 'UUtap failed to get');
         }
     }
 }
