61fb8c808a9f5798a1f58117983f4fe713268679
hesdin <hesdin01@gmail.com>
2023-06-07 11:42:49 +0800

    index

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Welcome to the API',
+        'version' => '1.0.0',
+    ]);
 });
